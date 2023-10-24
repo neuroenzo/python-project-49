38c3a8f44d78662e874f6f0703c89272999d62e0
neuroenzo <neuroenzo@gmail.com>
2023-10-24 23:44:35 +0300

    add indent in function

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,6 +8,7 @@ def prepare_gcd_game() -> str:
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     print('Question:', f'{number1} {number2}')
+
     while number1 != 0 and number2 != 0:
         if number1 > number2:
             number1 %= number2
