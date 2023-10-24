f7d37c5758ffde9fafe7c30b98914c73bc26ce01
neuroenzo <neuroenzo@gmail.com>
2023-10-24 23:41:10 +0300

    fix variable name

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,13 +9,13 @@ def launch_game(game: Callable[[], str]):
     user_name = welcome_user()
 
     for i in range(3):
-        answer = game()
+        correct_answer = game()
         user_answer = prompt.string('Your answer: ')
-        if answer == user_answer:
+        if correct_answer == user_answer:
             print('Correct')
         else:
             print(f'\'{user_answer}\' is wrong answer ;(.'
-                  f' Correct answer was \'{answer}\'.')
+                  f' Correct answer was \'{correct_answer}\'.')
             print(f'Let\'s try again, {user_name}!')
             return False
 
