c73d93c466652365303e65fc63008cbc417621fc
neuroenzo <neuroenzo@gmail.com>
2023-10-24 19:33:12 +0300

    simplify function

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,12 +3,9 @@ import random
 GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def prepare_game() -> str | None:
-    number = random.randint(1, 100)
+def prepare_game() -> str:
+    number = random.randint(2, 100)
     print(str('Question: 'f'{number}'))
-
-    if number <= 1:
-        return
 
     divider = 2
     for i in range(2, int(number / 2) + 1):
