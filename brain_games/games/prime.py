fd11c1f78227f3895baa7a1657846620572130d7
neuroenzo <neuroenzo@gmail.com>
2023-11-02 00:39:02 +0300

    fix game description

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 import random
 
-DESCRIPTION = 'Answer "yes" if the number is even,' \
-              ' otherwise answer "no".'
+DESCRIPTION = 'Answer "yes" if given number is prime.' \
+              ' Otherwise answer "no".'
 
 
 def get_question_and_answer() -> tuple:
