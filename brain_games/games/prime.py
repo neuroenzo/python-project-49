9c5ee446230025a29acf5c59a545f4e40e0df901
neuroenzo <neuroenzo@gmail.com>
2023-11-05 15:29:37 +0300

    move game logic to own method

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,15 +4,27 @@ DESCRIPTION = 'Answer "yes" if given number is prime.' \
               ' Otherwise answer "no".'
 
 
-def get_question_and_answer() -> tuple:
-    number = random.randint(2, 100)
-    question = number
+def is_prime(number):
+    if number < 2:
+        return False
 
     divider = 2
-    for i in range(2, int(number / 2) + 1):
+    for i in range(2, int(number // 2) + 1):
         if number % i == 0:
             divider += 1
 
-    answer = 'no' if divider > 2 else 'yes'
+    if divider <= 2:
+        return True
+    else:
+        return False
+
+
+def get_question_and_answer() -> tuple:
+    number = random.randint(1, 100)
+    question = number
+
+    divider = is_prime(number)
+
+    answer = 'yes' if divider else 'no'
 
     return question, answer
